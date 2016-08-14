c7d5f845c587c058e9e059bf4a425381f0c96ee4
Thomas GARGOT <Ofix@pc21.home>
2016-08-14 11:11:19 +0200

    import datasets

--- RTE_extraction.R
+++ RTE_extraction.R
@@ -1,1 +1,15 @@
-this is a test
+devtools::install_github("tutuchan/fodr")
+library("fodr")
+
+#List of the portals
+list_portals()
+
+# call the name of the portal in minuscule
+portal <- fodr_portal("rte")
+portal
+
+list_datasets <-  portal$search(q = "production")
+list_datasets[[1]]
+
+dts <- list_datasets[[1]]
+dts$get_records()
